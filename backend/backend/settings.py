451445847fcbc5a25e7269013dc01f25eddf2787
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-07-30 13:50:16 -0500

    cambio de debug a true para evitar error de static no encontrados

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qwme1_&t_3=4ww9&dtr(skt!rn$ithqm4&afs4k9a-=d_e-5l='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
