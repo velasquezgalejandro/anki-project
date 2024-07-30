828aefc08634521f16e47446ef070d5b2fa8e8ee
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-07-30 13:57:41 -0500

    se agrega axios (test)

--- frontend/src/decks/Decks.jsx
+++ frontend/src/decks/Decks.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
+import axios from 'axios';
+
+axios
+  .get('https://localhost:8000/decks')
+  .then((response) => {
+    console.log(response.data);
+    // Display fetched data
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
 
 const Decks = () => {
-  return <Stack sx={{ width: 1, height: 1, bgcolor: 'red' }}>Decks</Stack>;
+  return <Stack sx={{ width: 1, height: 1 }}>Decks</Stack>;
 };
 
 export default Decks;
