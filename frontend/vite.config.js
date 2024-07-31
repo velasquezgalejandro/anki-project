e136359b389a0dcb445a1666e5ec158dfeb5d83c
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-07-31 14:02:13 -0500

    se configura las opciones de vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    hmr: true,
+  },
+});
