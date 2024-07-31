d30fc883c43e960cc0cd92e458f98497119ae665
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-07-31 14:02:03 -0500

    se implementa vite al front

--- frontend/src/decks/Decks.jsx
+++ frontend/src/decks/Decks.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import Stack from '@mui/material/Stack';
 import axios from 'axios';
 
-axios
-  .get('https://localhost:8000/decks')
-  .then((response) => {
-    console.log(response.data);
-    // Display fetched data
-  })
-  .catch((error) => {
-    console.error('Error fetching data:', error);
-  });
+// axios
+//   .get('http://localhost:8000/decks/')
+//   .then((response) => {
+//     console.log(response.data);
+//     // Display fetched data
+//   })
+//   .catch((error) => {
+//     console.error('Error fetching data:', error);
+//   });
 
 const Decks = () => {
-  return <Stack sx={{ width: 1, height: 1 }}>Decks</Stack>;
+  return <Stack sx={{ width: 1, height: 1 }}>test test test</Stack>;
 };
 
 export default Decks;
