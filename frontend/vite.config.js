567dc46efb8e62499efa3a434d909179d4b07b16
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-08-01 13:47:46 -0500

    arreglo en vite para reflejar los cambios

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   server: {
     port: 3000,
     strictPort: true,
+    // host: '0.0.0.0',
     host: true,
     hmr: true,
+    watch: {
+      usePolling: true,
+    },
   },
 });
