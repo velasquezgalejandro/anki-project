774e6d5fb5aca81793fc5235192ce714022c6464
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-08-01 10:16:20 -0500

    cambio menor en vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 3000,
-    strictPort: true,
-  },
   server: {
     port: 3000,
     strictPort: true,
