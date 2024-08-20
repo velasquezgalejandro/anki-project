17181b83edbb789b56df1cc2ac7450240e1a24e6
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-08-20 10:37:51 -0500

    llamada a api para create decks #5

--- frontend/src/views/CreateDecksView.jsx
+++ frontend/src/views/CreateDecksView.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const CreateDecksView = () => {
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/categories/');
+      setData(response.data); // Guarda los datos en el estado
+    } catch (err) {
+      console.log({ err });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log({ data });
+
   return <div>CreateDecksView</div>;
 };
 
