b71e812e5460a41d85023392e6b9aa7995faccdd
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-08-20 13:53:16 -0500

    creacion de stepper para crear decks #5

--- frontend/src/views/CreateDecksView.jsx
+++ frontend/src/views/CreateDecksView.jsx
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+const steps = [
+  'Select campaign settings',
+  'Create an ad group',
+  'Create an ad',
+];
+
 const CreateDecksView = () => {
   const [data, setData] = useState();
-  const [error, setError] = useState(null);
+  const [activeStep, setActiveStep] = React.useState(0);
 
   const fetchData = async () => {
     try {
@@ -18,9 +30,25 @@ const CreateDecksView = () => {
     fetchData();
   }, []);
 
-  console.log({ data });
+  console.log(data);
 
-  return <div>CreateDecksView</div>;
+  return (
+    <Stack sx={{ p: 2 }}>
+      <Card sx={{ height: 1, p: 2, bgcolor: 'red', borderRadius: 2 }}>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+      </Card>
+    </Stack>
+  );
 };
 
 export default CreateDecksView;
