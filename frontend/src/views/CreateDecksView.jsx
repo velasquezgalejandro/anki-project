79db270d7711b2d248c3c907fe04b20866559eb3
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-08-21 13:58:42 -0500

    creacion de form para envio de decks front cargar decks #5

--- frontend/src/views/CreateDecksView.jsx
+++ frontend/src/views/CreateDecksView.jsx
@@ -2,47 +2,90 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Stack from '@mui/material/Stack';
-import Card from '@mui/material/Card';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-
-const steps = ['Crear Categoria', 'Crear Subcategoria', 'Crear Decks'];
+import { FormWrapper, GenericTextField, GenericAutocomplete } from '../utils';
 
 const CreateDecksView = () => {
-  const [data, setData] = useState();
-  const [activeStep, setActiveStep] = React.useState(0);
+  const [dataCategory, setDataCategory] = useState();
+  const [dataSubcategory, setDataSubcategory] = useState();
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedSubcategory, setSelectedSubcategory] = useState(null);
+  const [title, setTitle] = useState('');
 
-  const fetchData = async () => {
+  const fetchDataCategory = async () => {
     try {
       const response = await axios.get('http://localhost:8000/categories/');
-      setData(response.data); // Guarda los datos en el estado
+      setDataCategory(response.data);
     } catch (err) {
       console.log({ err });
     }
   };
 
+  const fetchDataSubCategory = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/subcategories/');
+      setDataSubcategory(response.data);
+    } catch (err) {
+      console.log({ err });
+    }
+  };
+
+
   useEffect(() => {
-    fetchData();
+    fetchDataCategory();
+    fetchDataSubCategory();
   }, []);
 
-  console.log(data);
+  // console.log({ dataCategory });
+  // console.log({ dataSubcategory });
+
+  const handleChangeAutocomplete = (setState, newValue) => {
+    setState(newValue);
+  };
+
+  const handleChangeTextField = (setState, event) => {
+    setState(event.target.value);
+  };
 
   return (
     <Stack sx={{ p: 2 }}>
-      <Card sx={{ height: 1, p: 2, bgcolor: 'red', borderRadius: 2 }}>
-        <Stepper activeStep={activeStep}>
-          {steps.map((label, index) => {
-            const stepProps = {};
-            const labelProps = {};
-            return (
-              <Step key={label} {...stepProps}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
-              </Step>
-            );
-          })}
-        </Stepper>
-      </Card>
+      <FormWrapper title="title">
+        <GenericTextField
+          label="Title"
+          name="title"
+          helperText={''}
+          value={title}
+          onChange={(e) => {
+            handleChangeTextField(setTitle, e);
+          }}
+        />
+        <GenericAutocomplete
+          label="category"
+          name="category"
+          options={dataCategory}
+          helperText={''}
+          value={selectedCategory}
+          onChange={(e) => handleChangeAutocomplete(setSelectedCategory, e)}
+        />
+        <GenericAutocomplete
+          label="subcategory"
+          name="subcategory"
+          options={dataSubcategory}
+          helperText={''}
+          value={selectedSubcategory}
+          onChange={(e) => {
+            handleChangeAutocomplete(setSelectedSubcategory, e);
+          }}
+        />
+        <GenericTextField
+          label="language"
+          name="lenguaje"
+          helperText={''}
+          value={''}
+          onChange={(e) => {
+            handleChangeTextField(setLanguage, e);
+          }}
+        />
+      </FormWrapper>
     </Stack>
   );
 };
