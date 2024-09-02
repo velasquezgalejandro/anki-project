fa172fc1e1f8c153e73a1916d40350a0dc54d7ee
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-09-02 13:56:01 -0500

    cambios de logica para leer y convertir archivos en cards

--- backend/decks/views.py
+++ backend/decks/views.py
@@ -3,9 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Decks, Cards
 from .serializers import DecksSerializer, CardsSerializer
-
-# from EbookLib import epub
 from django.core.files.uploadedfile import UploadedFile
+
+import io
+from ebooklib import epub
+from pyepub import EPUB
 
 # Create your views here.
 class DecksListCreateView(generics.ListCreateAPIView):
@@ -23,54 +25,77 @@ class EpubUploadView(APIView):
         print('----------------')
         print(epub_file)
         print('----------------')
+
         if epub_file and epub_file.content_type == 'application/epub+zip':
-            # try:
-            #     # Leer el archivo EPUB
-            #     file_content = epub_file.read()
+            try:
+                # # Leer el archivo EPUB
+                file_content = epub_file.read()
+                epub = EPUB(io.BytesIO(file_content))
 
-            #     # Procesar el archivo EPUB
-            #     book = epub.read_epub(io.BytesIO(file_content))
+                # # Extraer información básica
+                print('----------extraer informacion---------------------------')
+                print(epub)
+                print('----------extraer informacion---------------------------')
+# documents = []
+#                 for item in epub.get_items():
+#                     if item.get_type() == 'text':
+#                         documents.append(item.get_content().decode('utf-8'))
 
-            #     # Extraer información básica
-            #     title = book.get('title', 'Desconocido')
-            #     author = book.get('author', 'Desconocido')
+#                 # Preparar la respuesta
+#                 response_data = {
+#                     'title': title,
+#                     'author': author,
+#                     'documents': documents[:5]  # Limitar el número de documentos mostrados
+#                 }
+#                 return Response(response_data, status=status.HTTP_200_OK)
+                response_data = {}
 
-            #     # Puedes extraer más información si lo necesitas
-            #     documents = []
-            #     for item in book.items:
-            #         if item.get_type() == epub.EpubHtml:
-            #             documents.append(item.get_content().decode('utf-8'))
-
-            #     # Preparar la respuesta
-            #     response_data = {
-            #         'title': title,
-            #         'author': author,
-            #         'documents': documents[:5]  # Limitar el número de documentos mostrados
-            #     }
-            response_data = {}
-
-            return Response(response_data, status=status.HTTP_200_OK)
-            # except Exception as e:
-            #     # Imprime el rastreo del error en los logs
-            #     print("Exception: ", e)
-            #     return Response({'error': 'Failed to process EPUB file'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(response_data, status=status.HTTP_200_OK)
+            except Exception as e:
+                # Imprime el rastreo del error en los logs
+                print("Exception: ", e)
+                return Response({'error': 'Failed to process EPUB file'}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'error': 'Invalid file format'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-# from EbookLib import epub
+# class EpubUploadView(APIView):
+#     def post(self, request, *args, **kwargs):
+#         epub_file = request.FILES.get('file')
+#         print('----------------')
+#         print(epub_file)
+#         print('----------------')
+#         if epub_file and epub_file.content_type == 'application/epub+zip':
+#             try:
+#                 # Leer el archivo EPUB
+#                 file_content = epub_file.read()
 
-# def extract_epub_data(file):
-#     book = epub.read_epub(file)
+#                 # Procesar el archivo EPUB
+#                 book = epub.read_epub(io.BytesIO(file_content))
 
-#     # Extraer título y autor
-#     title = book.get_metadata('DC', 'title')
-#     author = book.get_metadata('DC', 'creator')
+#                 # Extraer información básica
+#                 print('----------extraer informacion---------------------------')
+#                 print(book)
+#                 print('----------extraer informacion---------------------------')
+#             #     title = book.get('title', 'Desconocido')
+#             #     author = book.get('author', 'Desconocido')
 
-#     # Extraer otros metadatos
-#     # Aquí puedes usar otros métodos disponibles en `book`
+#             #     # Puedes extraer más información si lo necesitas
+#             #     documents = []
+#             #     for item in book.items:
+#             #         if item.get_type() == epub.EpubHtml:
+#             #             documents.append(item.get_content().decode('utf-8'))
 
-#     return {
-#         'title': title[0] if title else 'Desconocido',
-#         'author': author[0] if author else 'Desconocido',
-#         # Agrega más datos según sea necesario
-#     }
+#             #     # Preparar la respuesta
+#             #     response_data = {
+#             #         'title': title,
+#             #         'author': author,
+#             #         'documents': documents[:5]  # Limitar el número de documentos mostrados
+#             #     }
+#                 response_data = {}
+
+#                 return Response(response_data, status=status.HTTP_200_OK)
+#             except Exception as e:
+#                 # Imprime el rastreo del error en los logs
+#                  print("Exception: ", e)
+#                  return Response({'error': 'Failed to process EPUB file'}, status=status.HTTP_400_BAD_REQUEST)
+#         return Response({'error': 'Invalid file format'}, status=status.HTTP_400_BAD_REQUEST)
